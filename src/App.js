424ee77e6a292294897fd8ffb8e1a34834711bce
elhadjissam20031 <elhadjissam20031@gmail.com>
2023-09-15 21:31:15 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Portfolio from "./pages/Portfolio";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainLayout from "./Layouts/MainLayout";
-import { RouterProvider, createBrowserRouter, createHashRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
 import Home from "./pages/Home";
 // import { BrowserRoute, Routes, Route } from "react-router-dom";
 
@@ -15,18 +19,24 @@ class App extends Component {
   constructor(props) {
     super(props);
   }
-  routes = createHashRouter([
+  routes = createHashRouter(
+    [
+      {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+          { index: true, element: <Home /> },
+          { path: "about", element: <About /> },
+          { path: "portfolio", element: <Portfolio /> },
+          { path: "contact", element: <Contact /> },
+        ],
+      },
+    ],
+
     {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-        { path: "home", element: <Home /> },
-        { path: "about", element: <About /> },
-        { path: "portfolio", element: <Portfolio /> },
-        { path: "contact", element: <Contact /> },
-      ],
-    },
-  ]);
+      basename: "/ass-one",
+    }
+  );
   state = {};
   render() {
     return (
